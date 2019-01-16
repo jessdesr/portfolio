79f19c8b166dcca0e1f4fa1e6f7194b783e89b01
Jess <shaderone.jd@gmail.com>
2019-01-16 01:50:54 -0800

    Update page titles and favicon

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,43 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Profile',
-      component: require('@/pages/Profile.vue').default
-    },
+const routes = [
+  {
+    path: '/',
+    name: 'Profile',
+    component: require('@/pages/Profile.vue').default,
+    meta: {
+      title: 'Jess Desrochers | Profile'
+    }
+  },
 
-    {
-      path: '/resume',
-      name: 'Resume',
-      component: require('@/pages/Resume.vue').default
-    },
+  {
+    path: '/resume',
+    name: 'Resume',
+    component: require('@/pages/Resume.vue').default,
+    meta: {
+      title: 'Jess Desrochers | Resume'
+    }
+  },
 
-    {
-      path: '/projects',
-      name: 'Projects',
-      component: require('@/pages/Projects.vue').default
-    },
-  ]
+  {
+    path: '/projects',
+    name: 'Projects',
+    component: require('@/pages/Projects.vue').default,
+    meta: {
+      title: 'Jess Desrochers | Projects'
+    }
+  },
+]
+
+const router = new Router({
+  routes,
 })
+
+// This callback runs before every route change, including on page load.
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title
+  next()
+})
+
+export default router
